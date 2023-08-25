8d6869cfc145c2d4573ad1444ab0aa969997bd96
Miguel Angel Seiz <mseizduque@gmail.com>
2023-08-25 20:35:39 +0200

    first commit

---
+++ animacion.js
@@ -0,0 +1,33 @@
+const desplazamiento = document.querySelector("#desplazamiento");
+
+function frames() {
+
+    const anim = desplazamiento.animate([
+        { transform: "TranslateY(0px)" },
+        { transform: `TranslateY(${screen.width > 1100 ? -5 : screen.width > 900 ? -13 : screen.width > 475 ? -15 : -18}vw)`},
+    ], {
+        easing: "linear",
+        iterations: 1,
+        duration: 800,
+    });
+
+    return anim.finished;
+}
+
+function displace() {
+
+    frames().then(() => {
+        desplazamiento.appendChild(document.querySelectorAll("#desplazamiento > li")[0]);
+    })
+}
+
+
+setInterval(() => {
+    displace();
+}, 5000);
+
+
+window.addEventListener("scroll", function(){
+    var navegador = document.querySelector("nav");
+    navegador.classList.toggle("abajo", window.scrollY > 0)
+})
